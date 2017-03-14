98c3e64d6e488b9af57697db9044746bc84e7bee
sunny <xiaoyao_xiao@126.com>
2017-03-14 15:41:19 +0800

    修改返回的菜单数组，使其正常，并且开启了登陆过滤

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	_ "go_admin/models"
 	_ "go_admin/routers"
 	"github.com/astaxie/beego"
-	//"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/context"
 )
 
 func init(){
-	//var FilterUser = func(ctx *context.Context) {
-	//	_, ok := ctx.Input.Session("sunny_user_uid").(int)
-	//	if !ok && ctx.Request.RequestURI != "/admin/login" {
-	//		ctx.Redirect(302, "/admin/login")
-	//	}
-	//}
-	//
-	//beego.InsertFilter("/*",beego.BeforeRouter,FilterUser)
+	var FilterUser = func(ctx *context.Context) {
+		_, ok := ctx.Input.Session("sunny_user_uid").(int)
+		if !ok && ctx.Request.RequestURI != "/admin/login" {
+			ctx.Redirect(302, "/admin/login")
+		}
+	}
+	
+	beego.InsertFilter("/*",beego.BeforeRouter,FilterUser)
 }
 
 func main() {
--- models/menuUrl.go
+++ models/menuUrl.go
@@ -50,68 +50,39 @@ func (menuurl *MenuUrl) GetMenuUrlById() (MenuUrl,error){
 	return *menuurl,err
 }
 
-func (MenuUrl *MenuUrl)GetTrees() []AllMenuUrl{
+func (menuurl *MenuUrl)GetTrees() []AllMenuUrl{
 	//获取所有module的菜单
 	module:=&Module{}
 	result,err:=module.GetAllModules(1)
 	//获取当前用户的菜单
-	//var access=[]int{1,5,108,112,113,122,109,114,115,123}
-	var newAllMenuUrl []AllMenuUrl
-	//var k=0
+	var access=[]int{1,5,108,112,113,122,109,114,115,123}
+	newAllMenuUrl:=make([]AllMenuUrl,0)
 	if err==nil {
 		for _, v := range result {
-			_, newerr := getListByModuleId(v.Module_id, "demo")
+			newresult, newerr := getListByModuleId(v.Module_id, "demo")
 			if (newerr == nil) {
-				newAllMenuUrl[0].Module_id=1
-				return newAllMenuUrl
-				//var newAllMenu []MenuUrl
-				//var newk=0
-				//for _,mv:=range newresult {
-				//	for _, v := range access {
-				//		if (mv.Menu_id==v) {
-				//			newAllMenu[newk].Menu_id=mv.Menu_id
-				//			newAllMenu[newk].Menu_name=mv.Menu_name
-				//			newAllMenu[newk].Menu_url=mv.Menu_url
-				//			newAllMenu[newk].Module_id=mv.Module_id
-				//			newAllMenu[newk].Is_show=mv.Is_show
-				//			newAllMenu[newk].Online=mv.Online
-				//			newAllMenu[newk].Shortcut_allowed=mv.Shortcut_allowed
-				//			newAllMenu[newk].Sort=mv.Sort
-				//			newAllMenu[newk].Menu_desc=mv.Menu_desc
-				//			newAllMenu[newk].Father_menu=mv.Father_menu
-				//			newk++
-				//			break
-				//		}else{
-				//			continue
-				//		}
-				//	}
-				//}
-				//newAllMenuUrl[k].Module_id=v.Module_id
-				//newAllMenuUrl[k].Module_name=v.Module_name
-				//newAllMenuUrl[k].Module_url=v.Module_url
-				//newAllMenuUrl[k].Module_sort=v.Module_sort
-				//newAllMenuUrl[k].Module_desc=v.Module_desc
-				//newAllMenuUrl[k].Module_icon=v.Module_icon
-				//newAllMenuUrl[k].Online=v.Online
-				//newAllMenuUrl[k].MenuList=newresult
-				//k++
+				newMenus:=make([]MenuUrl,0)
+				for _,mv:=range newresult {
+					for _, v := range access {
+						if (mv.Menu_id==v) {
+							newMenus=append(newMenus,mv)
+							break
+						}else{
+							continue
+						}
+					}
+				}
+				newModule:=AllMenuUrl{v.Module_id,v.Module_name,v.Module_url,v.Module_sort,v.Module_desc,v.Module_icon,v.Online,newMenus}
+				newAllMenuUrl=append(newAllMenuUrl,newModule)
 			} else {
 				continue
 			}
 		}
-	        return nil
+	        return newAllMenuUrl
 	}else{
-		return nil
+		return newAllMenuUrl
 	}
 }
-
-//func (menuUrl *MenuUrl)GetMenusByRole(roles string) ([]MenuUrl,error){
-//	o := orm.NewOrm()
-//	o.Using("default")
-//	var menus []MenuUrl
-//	_,err:= o.Raw("SELECT * FROM "+beego.AppConfig.String("mysqldbprefix")+"menu_url  WHERE online=1 AND menu_id in (?)",roles).QueryRows(&menus)
-//	return menus,err
-//}
 
 func getListByModuleId(id int,flag string)([]MenuUrl,error){
 	o := orm.NewOrm()
